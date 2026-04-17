45cfd4e9717e86e06e37aae39305ade7f1d28219
英文名 <>
2026-04-17 10:01:04 +0800

    123456

---
+++ guess.js
@@ -0,0 +1,4 @@
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+const result = document.querySelector(".result");
+const count = document.querySelector(".count");
